5aed4998a00945e7c25ac1ef7b5625b561fdc78e
igneH <alexander1pixner@gmail.com>
2023-09-15 22:15:24 +0200

    added 2 GET commands

--- Controllers/ItemsController.cs
+++ Controllers/ItemsController.cs
@@ -1,9 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Rest_API_Tutorial.Entities;
 using Rest_API_Tutorial.Repositories;
+using System.Collections.Generic;
 
 namespace Rest_API_Tutorial.Controllers
 {
@@ -11,6 +10,28 @@ namespace Rest_API_Tutorial.Controllers
     [Route("items")]
     public class ItemsController : ControllerBase
     {
-        private readonly InMemItemsRepository repository;
+        private readonly InMemItemsRepository _repository;
+
+        public ItemsController()
+        {
+            _repository = new InMemItemsRepository();
+        }
+
+        //GET /items
+        [HttpGet]
+        public IEnumerable<Item> GetItems()
+        {
+            var items = _repository.GetItmes();
+            return items;
+        }
+
+        //GET /items/{id}
+        [HttpGet("{id}")]
+        public ActionResult<Item> GetItem(Guid id)
+        {
+            var item = _repository.GetItem(id);
+
+            return item == null ? NotFound() : item;
+        }
     }
 }
